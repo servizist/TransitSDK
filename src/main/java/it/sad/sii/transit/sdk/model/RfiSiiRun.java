e44b876848c5899527fb7f0ed437a65e7b083210
lconcli <lisa.concli@servizist.it>
2019-06-24 10:53:15 +0200

    r1.6.0 - real commit

--- src/main/java/it/sad/sii/transit/sdk/model/RfiSiiRun.java
+++ src/main/java/it/sad/sii/transit/sdk/model/RfiSiiRun.java
@@ -8,22 +8,50 @@ import java.io.Serializable;
 
 public class RfiSiiRun implements Serializable {
     private static final long serialVersionUID = 1L;
-    
+
+    private String RFICode;
     private String lineId;
     private String runId;
     private String carrierId;
 
-    public RfiSiiRun(String lineId, String runId, String carrierId) {}
+    public RfiSiiRun() {}
+
+    public RfiSiiRun(String RFICode, String lineId, String runId, String carrierId) {
+        this.RFICode = RFICode;
+        this.lineId = lineId;
+        this.runId = runId;
+        this.carrierId = carrierId;
+    }
+
+    public String getRFICode() {
+        return RFICode;
+    }
+
+    public void setRFICode(String RFICode) {
+        this.RFICode = RFICode;
+    }
 
     public String getLineId() {
         return lineId;
+    }
+
+    public void setLineId(String lineId) {
+        this.lineId = lineId;
     }
 
     public String getRunId() {
         return runId;
     }
 
+    public void setRunId(String runId) {
+        this.runId = runId;
+    }
+
     public String getCarrierId() {
         return carrierId;
+    }
+
+    public void setCarrierId(String carrierId) {
+        this.carrierId = carrierId;
     }
 }
